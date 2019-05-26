ca22ba6040e1c02fb267115e52917d0743c878c2
StevenChen <o0icegroup0o@gmail.com>
2019-05-26 09:34:48 +0800

    Initial commit

---
+++ src/main/java/com/icegroup/lpr/LprServiceApplication.java
@@ -0,0 +1,13 @@
+package com.icegroup.lpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LprServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LprServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icegroup/lpr/LprServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icegroup.lpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LprServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
